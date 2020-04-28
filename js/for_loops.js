913831264151e5c69ad5092f8f95bf4a1d0f4dad
Chris Eckert <chris.eckert08@gmail.com>
2020-04-28 16:15:29 -0500

    js for loops exercises

---
+++ js/for_loops.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// ----------Question 1----------
+// function showMultiplicationTable(num) {
+// 	for (var i = 1; i <= 10; i++) {
+// 		console.log(num + " x " + i + " = " + (num * i));
+// 	}
+// }
+//
+// showMultiplicationTable(5);
+
+// ----------Question 2----------
+// var randomNumber = Math.floor(Math.random() * 180) + 20;
+
+// function evenOrOdd(num) {
+// 	return (num % 2 === 0) ? num + " is an even number" : num + " is an odd number";
+// }
+//
+// for (var i = 0; i < 10; i++) {
+// 	var randomNumber = Math.floor(Math.random() * 180) + 20;
+// 	console.log(evenOrOdd(randomNumber));
+// }
+
+// ----------Question 3----------
+
+// for (var i = 1; i < 10; i++) {
+// 	var str = "";
+// 	for (var j = 1; j <= i; j++) {
+// 		str += i;
+// 	}
+// 	console.log(str);
+// }
+
+// ----------Question 4----------
+
+// for (var i = 100; i > 0; i--) {
+// 	if (i % 5 === 0) {
+// 		console.log(i);
+// 	}
+// }
+
+// ----------Question 3----------
