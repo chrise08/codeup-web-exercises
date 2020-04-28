6e9e240fa7432964ead50dcbd16e81a35ad14dba
Chris Eckert <chris.eckert08@gmail.com>
2020-04-28 16:19:58 -0500

    js loops exercises continued

---
+++ js/break_and_continue.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// function getOddNumber() {
+// 	var num = parseFloat(prompt("Please give me an odd number between 1 and 50."));
+// 	if (num % 2 === 0 && num < 1 && num > 50) {
+// 		alert("Sorry, that's not a valid number!");
+// 		parseFloat(prompt("Please give me an odd number between 1 and 50."));
+// 	} else {
+// 		alert("Thank you.");
+// 	}
+// }
+//
+// var oddNumber = getOddNumber();
+//
+// do {
+// 	parseFloat(prompt("Please give me an odd number between 1 and 50."));
+// } while ()
+
+
+
