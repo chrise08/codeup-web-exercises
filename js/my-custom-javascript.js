0c5540feb4724ddbb4c34db62fa1fefe35f44bde
Chris Eckert <chris.eckert08@gmail.com>
2020-05-12 13:46:38 -0500

    jquery selectors exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -4,7 +4,21 @@
 // (function ($) {
 
 $(document).ready(function() {
-	alert( 'The DOM has finished loading!' );
+	// alert( 'The DOM has finished loading!' );
+	
+	// var contents = $('#main-header').html();
+	//
+	// alert(contents);
+	
+	// $('.codeup').css('border', 'solid 1px red');
+	
+	$('li').css('font-size', '20px');
+	
+	$('h1, p, li').css('background-color', 'yellow');
+	
+	var header = $('h1').html();
+	
+	alert(header);
 });
 
 // })(jQuery);
