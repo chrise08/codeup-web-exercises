24b07c9e44888542cf76ff867263d84384fa5858
Chris Eckert <chris.eckert08@gmail.com>
2020-05-22 15:56:50 -0500

    promises lec notes

---
+++ js/promises-lec.js
@@ -0,0 +1,97 @@
+"use strict";
+
+/*********************************************
+ *        USING FETCH WITH PROMISES
+ ******************************************** */
+
+var pokemonPromise = fetch('https://pokeapi.co/api/v2/pokemon');
+// return of fetch request is a Promise object
+
+//TODO: console log the results of the above Promise. What is the result?
+
+console.log(pokemonPromise);
+
+//TODO: add a method that runs if the Promise resolves successfully
+
+const listNames = (arr => arr.forEach(element => console.log(element.name))); // way to create a reusable function to get the array element names
+
+pokemonPromise.then(response => response.json())
+    // the return from this first function, is passed as a parameter in the second one (next line)
+    .then(jsonResponse => jsonResponse.results)
+    .then(listNames)
+    .catch(error => console.log(error));
+
+//TODO: add a method that runs if the Promise fails
+
+//TODO: In the callback function of the .then method, parse the response into JSON
+
+/*********************************************
+ *              CHAINING PROMISES
+ ******************************************** */
+
+// TODO: Using Promise chaining, add another .then method that returns the results property of
+//  the first returned response.
+//    Taking a look at this second return, what are the results? What is the shape of our data?
+
+// TODO: Finally, chain another .then method that would log all of the name properties of the
+//  returned pokemon.
+// BONUS: Is there a way for us to clean up our code?
+
+// Let's try working with the Star Wars API!
+
+let starWarsAPI = fetch("https://swapi.dev/api/films/");
+
+// Parse the response into json
+starWarsAPI.then(response => response.json())
+    // access the results property from the json object
+    .then(jsonResponse => jsonResponse.results)
+    // iterate over the results array, and access title and id properties of each film
+    .then(resultsArr => resultsArr.forEach(film => {
+        console.log(film.title + ' ' + film.episode_id)
+    }))
+    // this runs if the fetch fails
+    .catch(error => console.log(error));
+
+// TODO: Using Promises, make a fetch request to the Star Wars API
+
+// TODO: Use Promise chaining to console log the json response
+
+// TODO: chain another method that iterates through the results array and console logs the names
+//  of all characters and their birth_years
+
+/*********************************************
+ *              CUSTOM PROMISES
+ ******************************************** */
+
+// To further understand Promises, we can create our own custom Promise object
+// Let's take a look at the example from the curriculum
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (Math.random() > 0.5) {
+            resolve('This is where the API data would be');
+        } else {
+            reject('Your API request failed');
+        }
+    }, 1500);
+});
+
+myPromise.then(response => console.log('resolved!'))
+         .catch(error => console.log('rejected!'));
+
+// The resolve and reject functions can also be passed a value.
+// These can then be used in our .resolve and .reject methods
+
+// TODO: pass in a message argument into the resolve and reject functions
+// TODO: use the passed in arguments in the callback functions
+
+// This is a simplified example of what goes on underneath the hood of a more complex fetch
+// request. The concepts we covered are similar to what you learned when using AJAX with jQuery.
+
+Promise.all([pokemonPromise, starWarsAPI])
+    .then(data => console.log(data));
+
+Promise.race([pokemonPromise, starWarsAPI])
+    .then(data => console.log(data));
+
+
